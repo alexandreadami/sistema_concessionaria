644a1e7c4367288cd39d835b787c0f3f565268b9
ALEXANDRE ADAMI <55349862120@jpmorgan.sp>
2017-10-27 21:22:42 -0200

    21:23

--- Cliente.cs
+++ Cliente.cs
@@ -9,12 +9,38 @@ public class Cliente{
     public void CapturarDados(){
         Console.Write("Informe seu nome: ");
         nome = Console.ReadLine();
-        Console.Write("Infome seu CPF: ");
-        cpf = Console.ReadLine();
+        bool cpfvalido = false;
+        Validacao validadocumento = new Validacao();
+        
+        do
+        {
+            Console.Write("Infome seu CPF: ");
+            cpf = Console.ReadLine();
+            cpfvalido = validadocumento.ValidarCPF(cpf);
+
+            if (cpfvalido)
+            {
+                Console.WriteLine();
+                //Console.WriteLine("cpf valido");
+                //Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Cpf invalido");
+            }
+
+            
+        } while (!cpfvalido);
+        
+        
+        
+        
+        
         
     }
     
-
+    
 
 
  }
